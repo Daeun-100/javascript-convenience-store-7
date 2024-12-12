f83437be16f9820082d5dca2bdf0f95f1494e45a
Ko Daeun <drechsu@gmail.com>
2024-12-12 19:28:27 +0900

    feat(OutputView) : 환영인사 출력

---
+++ src/view/OutputView.js
@@ -0,0 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export default class OutputView {
+  static printGreeting() {
+    Console.print("안녕하세요. 편의점입니다");
+    Console.print("현재 구매 가능 목록 출력합니다");
+  }
+  static printStock(stocks) {}
+}
